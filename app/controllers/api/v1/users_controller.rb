31aef1a6ad7ccd7abcc1898aa9df67cbea9ce567
dhruvika.k@simformsolutions.com <dhruvika.k@simformsolutions.com>
2021-10-21 15:32:30 +0530

    active_model_serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
     def index
         @users = User.all
-        render json: @users
+        render json: @users, adpter: :json
     end
 
     def show
